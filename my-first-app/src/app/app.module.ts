027e4c0ee83490091efb5db888b311649b323d2a
Robert Checco <Robert@lergent.com>
2017-06-21 13:03:53 -0400

    Add Farm Component

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { FarmComponent } from './farm/farm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    FarmComponent
   ],
   imports: [
     BrowserModule,
