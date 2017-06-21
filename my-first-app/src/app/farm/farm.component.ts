806b51e7847131cc7cc3290126cf3894c0c7db84
Robert Checco <Robert@lergent.com>
2017-06-21 16:30:02 -0400

    Began to cover directives

--- my-first-app/src/app/farm/farm.component.ts
+++ my-first-app/src/app/farm/farm.component.ts
@@ -4,16 +4,31 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-farm]',
   // selector: '.app-farm',
   selector: 'app-farm',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './farm.component.html',
   styleUrls: ['./farm.component.css']
 })
 export class FarmComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  serverCreated = false;
 
-  constructor() { }
+  constructor() {
+  	setTimeout(() => {
+  		this.allowNewServer = true;
+
+  	}, 2000);
+   }
 
   ngOnInit() {
+
+  }
+  onCreateServer(){
+  	this.serverCreated = true;
+  	this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
+  onUpdateServerName(event: any){
+  	this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
